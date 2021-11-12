d89f403b32fe301d68ec5052f327cc54fea45918
ngkiatran1910 <nghiahtcon@gmail.com>
2021-11-12 14:09:04 +0700

    QuanLyKaraoke

---
+++ QuanLyKaraoke_Nhom999/src/Services/DichVuService.java
@@ -0,0 +1,29 @@
+
+package Services;
+
+import java.util.List;
+
+
+public class DichVuService extends IServices.IServiceDichVu{
+
+    @Override
+    public void them(Object model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void sua(Object model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object selectByID(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ QuanLyKaraoke_Nhom999/src/Services/PhongService.java
@@ -0,0 +1,30 @@
+
+package Services;
+
+
+import java.util.List;
+
+
+public class PhongService extends IServices.IServicePhong{
+
+    @Override
+    public void them(Object model) {
+        
+    }
+
+    @Override
+    public void sua(Object model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object selectByID(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
