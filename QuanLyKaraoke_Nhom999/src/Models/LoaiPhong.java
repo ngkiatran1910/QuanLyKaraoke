9bda1145bb6e3985cfedb1fecd9c2d4fe9d0d0cf
thienndph18157 <thienndph18157@fpt.edu.vn>
2021-11-13 22:06:25 +0700

    QuanLyKaraoke

---
+++ QuanLyKaraoke_Nhom999/src/Models/LoaiPhong.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Admin
+ */
+public class LoaiPhong {
+    public String MaLP,TenLP;
+    public float  GiaTheoGio;
+
+    public LoaiPhong() {
+    }
+
+    public LoaiPhong(String MaLP, String TenLP, float GiaTheoGio) {
+        this.MaLP = MaLP;
+        this.TenLP = TenLP;
+        this.GiaTheoGio = GiaTheoGio;
+    }
+
+    public String getMaLP() {
+        return MaLP;
+    }
+
+    public void setMaLP(String MaLP) {
+        this.MaLP = MaLP;
+    }
+
+    public String getTenLP() {
+        return TenLP;
+    }
+
+    public void setTenLP(String TenLP) {
+        this.TenLP = TenLP;
+    }
+
+    public float getGiaTheoGio() {
+        return GiaTheoGio;
+    }
+
+    public void setGiaTheoGio(float GiaTheoGio) {
+        this.GiaTheoGio = GiaTheoGio;
+    }
+
+    @Override
+    public String toString() {
+        return "LoaiPhong{" + "MaLP=" + MaLP + ", TenLP=" + TenLP + ", GiaTheoGio=" + GiaTheoGio + '}';
+    }
+    
+}
